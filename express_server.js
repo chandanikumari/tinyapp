faa683fe7fd303157c900170543d615613f07b79
Chandani Kumari <chandaanikumari92@gmail.com>
2023-08-08 18:55:32 -0700

    added  generateRandomString function & urls_new

--- express_server.js
+++ express_server.js
@@ -5,8 +5,19 @@ const app = express();
 const PORT = 8080; // default port 8080
 
 app.use(morgan('dev'));
+app.use(express.urlencoded({ extended: true }));
+
 
 app.set("view engine", "ejs");
+const generateRandomString = function() {
+  const alphanum = "123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+  let str = "";
+  for (let i = 0; i < 6; i++) {
+    let StrIndex = Math.floor(Math.random() * 61);
+    str += alphanum[StrIndex];
+  }
+  return str;
+};
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -30,12 +41,18 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:id", (req, res) => {
-  const templateVars = {
-    id: req.params.id,
-    longURL: urlDatabase[req.params.id] /* What goes here? */
-  };
+  const templateVars = {id: req.params.id, longURL: urlDatabase[req.params.id]};
   res.render("urls_show", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
 });
 
 app.listen(PORT, () => {
